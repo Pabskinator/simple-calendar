56fd72e7c6c684257070c9756a86d4a894aa126c
Adrian Pabe <adrianpabs14@gmail.com>
2020-12-23 23:35:50 +0800

    Add calendar and form components

---
+++ resources/js/components/AddEvent.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <form @submit.prevent="submitEvent">
+            <div class="form-group">
+                <label for="eventName">Event</label>
+                <input type="text" class="form-control" id="eventName">
+            </div>
+            <div class="form-row">
+                <div class="col">
+                    <label for="fromDate">From</label>
+                    <input type="date" class="form-control" id="fromDate">
+                </div>
+                <div class="col">
+                    <label for="toDate">To</label>
+                    <input type="date" class="form-control" id="toDate">
+                </div>
+            </div>
+            <div class="form-check form-check-inline mt-3" v-for="day in days" :key="day.name">
+                <input
+                    v-model="event.days"
+                    type="checkbox"
+                    class="form-check-input"
+
+                    :id="day.name"
+                    :value="day.value">
+
+                <label class="form-check-label" :for="day.name">{{ day.name }}</label>
+            </div>
+            <div class="mt-2">
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+
+            return {
+                days: [
+                    {
+                        name: 'Sun',
+                        value: 0
+                    },
+                    {
+                        name: 'Mon',
+                        value: 1
+                    },
+                    {
+                        name: 'Tue',
+                        value: 2
+                    },
+                    {
+                        name: 'Wed',
+                        value: 3
+                    },
+                    {
+                        name: 'Thu',
+                        value: 4
+                    },
+                    {
+                        name: 'Fri',
+                        value: 5
+                    },
+                    {
+                        name: 'Sat',
+                        value: 6
+                    },
+                ],
+
+                event: {
+                    to: '',
+                    from: '',
+                    days: [],
+                    name: '',
+                }
+            }
+
+        },
+
+        methods: {
+            submitEvent(){
+                console.log('submit event')
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Calendar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div v-for="month in events.months_and_days" :key="month.month_name">
+            <h2 class="font-weight-bolder">{{ month.month_name }}</h2>
+
+            <div
+                v-for="(date, index) in month.dates"
+                class="border-top border-bottom p-2"
+
+                :class="{'bg-success': date.active}"
+                :key="index">
+
+                <div class="row">
+                    <div class="col-md-2">
+                        {{ date.date }}
+                    </div>
+                    <div class="col-md-10">
+                        <span v-if="date.active">{{ date.name }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            events: {
+                type: Object,
+                default: {
+                    events: [],
+                    months_and_days: [{
+                        month_name: '',
+                        dates: []
+                    }]
+                }
+            }
+        },
+
+        data() {
+            return {
+
+            }
+        },
+
+        methods: {},
+    }
+</script>
